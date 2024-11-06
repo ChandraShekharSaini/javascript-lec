668bb1d413417a8966c3d602c1121fc8482d6dc1
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2024-11-06 19:52:37 +0530

    Added folder FunctionArgument

---
+++ FunctionArguments/1_defaultAegument.js
@@ -0,0 +1,11 @@
+
+function Person(name = "chandra") {
+    console.log("I am ", name)
+    return "Jahil"
+}
+
+let a = Person()
+console.log(a)
+Person("Babu ji Maharam")
+
+window.alert("bnmmmm")
